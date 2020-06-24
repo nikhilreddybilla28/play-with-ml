49cee121eceab2b68014d72230a0472723819319
Nikil Reddy <44868474+nikhilreddybilla28@users.noreply.github.com>
2020-06-24 20:44:32 +0530

    Update dobby.py

--- dobby.py
+++ dobby.py
@@ -22,8 +22,8 @@ import xgboost
 def main():
 	"""ML App made  with Streamlit """    
     
-	activities = ["EDA &VIZ" , "Modelling"]	
-	choice = st.sidebar.selectbox("Select Activities",activities)
+    activities = ["EDA &VIZ" , "Modelling"]	
+    choice = st.sidebar.selectbox("Select Activities",activities)
     
     if choice == 'EDA &VIZ':
         st.title('Play with ML')
@@ -34,35 +34,35 @@ def main():
         
         data = st.file_uploader("Upload a Dataset", type=["csv", "txt", "xlsx"])
         
-		if data is not None:
+	if data is not None:
             
             st.markdown('EDA')
-			df = pd.read_csv(data)
+	    df = pd.read_csv(data)
             st.dataframe(df.head())
             st.write('shape:',df.shape)
         
-			if st.checkbox("Show Columns"):
-				all_columns = df.columns.to_list()
-				st.write(all_columns)
+	    if st.checkbox("Show Columns"):
+	        all_columns = df.columns.to_list()
+		st.write(all_columns)
                 
             if st.checkbox("Null values"):
                 st.write(df1.isnull().sum())
                 
             if st.checkbox("Information"):
-				st.write(df.info())
+		st.write(df.info())
                 
-			if st.checkbox("Summary"):
-				st.write(df.describe())
+	    if st.checkbox("Summary"):
+		st.write(df.describe())
                 
             if st.checkbox("Show Selected Columns"):
                 all_columns_names = df.columns.tolist()
                 selected_columns = st.multiselect("Select Columns",all_columns)
-				df1 = df[selected_columns]
-				st.dataframe(df1)
+		df1 = df[selected_columns]
+		st.dataframe(df1)
                 
             if st.checkbox("Correlation Plot(Seaborn)"):
                 st.write(sns.heatmap(df.corr(),annot=True))
-				st.pyplot()
+		st.pyplot()
                 
             st.markdown('Data Visualization')
             if st.checkbox("Show Value Counts"):
@@ -74,15 +74,15 @@ def main():
                 selected_columns_names = st.multiselect("Select Columns To Plot",all_columns_names)
                 
                 if st.button("Generate Plot"):
-				    st.success("Generating   {} plot  for {}".format(type_of_plot,selected_columns_names))
+		    st.success("Generating   {} plot  for {}".format(type_of_plot,selected_columns_names))
 
                     if type_of_plot == 'area':
                         cust_data = df[selected_columns_names]
-					    st.area_chart(cust_data)
+		        st.area_chart(cust_data)
                         
-				    elif type_of_plot == 'bar':
-					    cust_data = df[selected_columns_names]
-					    st.bar_chart(cust_data)
+		    elif type_of_plot == 'bar':
+		        cust_data = df[selected_columns_names]
+			st.bar_chart(cust_data)
                 
                     '''elif type_of_plot == 'pie':
 					    cust_data = df[selected_columns_names]
@@ -92,27 +92,27 @@ def main():
 					        st.pyplot()'''
                     
                     elif st.checkbox("Pie Plot"):
-				        column_to_plot = st.selectbox("Select 1 Column",selected_columns_names)
-				        pie_plot = df[column_to_plot].value_counts().plot.pie(autopct="%1.1f%%")
-				        st.write(pie_plot)
-				        st.pyplot() 
+		        column_to_plot = st.selectbox("Select 1 Column",selected_columns_names)
+			pie_plot = df[column_to_plot].value_counts().plot.pie(autopct="%1.1f%%")
+			st.write(pie_plot)
+			st.pyplot() 
                     
-				    elif type_of_plot == 'line':
-					    cust_data = df[selected_columns_names]
-					    st.line_chart(cust_data)
+		    elif type_of_plot == 'line':
+	                cust_data = df[selected_columns_names]
+			st.line_chart(cust_data)
                     
                     elif type_of_plot == 'altair_chart':
-					    a = st.selectbox("Select X axis",all_columns)
+			a = st.selectbox("Select X axis",all_columns)
                         b = st.selectbox("Select Y axis",all_columns)
                         c = st.selectbox("Select a column ",all_columns)
                         c = alt.Chart(cust_data).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-					    st.altair_chart(c, use_container_width=True)
+			st.altair_chart(c, use_container_width=True)
 
 				# Custom Plot 
-				    elif type_of_plot:
-					    cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
-					    st.write(cust_plot)
-					    st.pyplot()
+		     elif type_of_plot:
+		         cust_plot= df[selected_columns_names].plot(kind=type_of_plot)
+			 st.write(cust_plot)
+			 st.pyplot()
                 
 choice == 'Modelling':   
     pass
